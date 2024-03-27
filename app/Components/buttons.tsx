dd295cc6b0a7949db78c9f2b2bc0f503eca72d7c
Gu <Goooleon@gmail.com>
2024-03-27 23:40:37 +0800

    test

--- app/Components/buttons.tsx
+++ app/Components/buttons.tsx
@@ -5,3 +5,5 @@ function DownloadButton() {
         </div>
     );
   }
+
+  export default DownloadButton;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from 'next/link';
-import DownloadButton from "./components/components";
+import DownloadButton from "./Components/buttons";
 
 export default function Home() {
   return (

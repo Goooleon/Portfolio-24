b99da157c2aa850a7157ea340a7341f699f2cabc
Gu <Goooleon@gmail.com>
2024-05-12 01:52:35 +0800

    Revert "icon update for apple"
    This reverts commit 0ebc3f6a386f1ae73cf6357a0f62a23922602f4d.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import DownloadButton from './Components/buttons';
 import ProjectTile from './Components/project-tile';
+import classNames from 'classnames';
 
 export default function Home() {
   return (

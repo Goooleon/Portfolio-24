f6d7da435edbb5bd81fe778a9b80178233de5d12
Gu <Goooleon@gmail.com>
2024-05-12 01:18:10 +0800

    revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,21 +13,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="icon"
-          href="/icon.svg"
-          type="image/svg+xml"
-          sizes="any"
-        />
-        <link
-          rel="apple-touch-icon"
-          href="/apple-icon.png"
-          type="image/png"
-          sizes="180x180"
-        />
-      </head>
       <body className={inter.className}>
           <>
             {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import type { Metadata } from "next";
+import Image from 'next/image';
+import Link from 'next/link';
+import DownloadButton from './Components/buttons';
 import ProjectTile from './Components/project-tile';
-
-export const metadata: Metadata = {
-  title: "Shengfeng Gu · Designer",
-  description: "Discover the portfolio of Shengfeng Gu (a.k.a. Gü), a Product Designer dedicated to crafting meaningful digital experiences. Explore a curated selection of works that showcase his unique approach and personality.",
- };
+import classNames from 'classnames';
 
 export default function Home() {
   return (

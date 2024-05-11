5b7d41134f32166d3bbc21d6e9e1ee4041772132
Gu <Goooleon@gmail.com>
2024-05-12 01:09:06 +0800

    loading page fix

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,21 +1,7 @@
-'use client'
+import SplashScreen from "./Components/splash";
 
-import React from "react";
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-
-const SplashScreen = () => {
+export default function Loading() {
     return (
-        <div className="flex items-center justify-center w-full h-dvh text-center bg-black">
-            <div className="h-32 w-32">
-                <DotLottieReact
-                    src="/splash-screen.lottie"
-                    loop
-                    autoplay
-                    onError={(e) => console.error('Error loading Lottie:', e)}
-                />
-            </div>
-        </div>
+        <SplashScreen />
     );
 };
-
-export default SplashScreen;

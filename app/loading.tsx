b10698454c674072fb387e99d740818154b326a2
Gu <Goooleon@gmail.com>
2024-05-12 20:58:04 +0800

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,11 @@
 import SplashScreen from "./Components/splash";
 
 export default function Loading() {
+    console.log("Loading component rendered"); // This will show in your browser console
+
     return (
         <div className="flex flex-col gap-12 px-12 items-center justify-center w-full h-dvh text-center">
             <SplashScreen />
-            <h1 className="text-2xl">{`test message`}</h1>
         </div>
     );
 }

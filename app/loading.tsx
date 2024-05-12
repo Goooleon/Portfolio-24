da514697591249eed72ff9e5a766732ebaf824d7
Gu <Goooleon@gmail.com>
2024-05-12 20:50:57 +0800

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import SplashScreen from "./Components/splash";
+
+export default function Loading() {
+    return (
+        <div className="flex flex-col gap-12 px-12 items-center justify-center w-full h-dvh text-center">
+            <SplashScreen />
+            <h1 className="text-2xl">{`test message`}</h1>
+        </div>
+    );
+}

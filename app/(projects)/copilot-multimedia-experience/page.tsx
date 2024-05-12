7dd59081b4390c48cef5668bcbd87872f267ba92
Gu <Goooleon@gmail.com>
2024-05-12 11:18:34 +0800

    adding lottie to copilot page

--- app/(projects)/copilot-multimedia-experience/page.tsx
+++ app/(projects)/copilot-multimedia-experience/page.tsx
@@ -1,3 +1,4 @@
+import SplashScreen from "@/app/Components/splash";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -8,6 +9,7 @@ export const metadata: Metadata = {
 export default function Project() {
     return (
         <div>
+            <SplashScreen />
             <h1>Copilot Multimedia Experience</h1>
             <p>Follow on Edge is an experimental project aimed at expanding Edge&apos;s web services by allowing users to follow any content on the web, focusing more effectively on their interests. This project introduces the MVP version of Follow on Edge, supporting the ability to follow creators on YouTube, TikTok, and Pinterest.</p>
         </div>

37a92dff082a28408de4eadbd8273c92986ee7cb
Gu <Goooleon@gmail.com>
2024-05-12 22:14:46 +0800

    bugfix

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
     console.log("Loading component rendered"); // This will show in your browser console
 
     return (
-        <div className="flex flex-col gap-12 px-12 items-center justify-center w-full h-dvh text-center">
+        <div className="flex items-center justify-center w-full h-dvh">
             <SplashScreen />
         </div>
     );

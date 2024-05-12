b81e8ad2d950143f191e65e315a3b79e61d4dc1a
Gu <Goooleon@gmail.com>
2024-05-12 11:15:12 +0800

    adding a splash component only

---
+++ app/Components/splash.tsx
@@ -0,0 +1,17 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+ 
+const SplashScreen = () => {
+    return (
+        <div className="flex flex-col gap-12 px-12 items-center justify-center w-full h-dvh text-center bg-black">
+            <div className="w-40 h-40">
+                <DotLottieReact
+                    src="/splash-screen.lottie"
+                    loop
+                    autoplay
+                />
+            </div>
+        </div>
+    );
+};
+ 
+export default SplashScreen;
